cccde32cd9a78145e5dec5b63e578d1537a23619
xzh <wanshuang2011@ncist.edu.cn>
2023-12-25 22:33:13 +0800

    23-12-25更新
    1.修改dotinside函数

--- 完整项目代码/Renderer(vc)/my_models/face.cpp
+++ 完整项目代码/Renderer(vc)/my_models/face.cpp
@@ -13,4 +13,14 @@ namespace XX_XZH
   {
     std::cout << vertex_index[0] << "/" << vertex_texture_index[0] << "/" << vertex_normal_index[0] << " " << vertex_index[1] << "/" << vertex_texture_index[1] << "/" << vertex_normal_index[1] << " " << vertex_index[2] << "/" << vertex_texture_index[2] << "/" << vertex_normal_index[2] << std::endl;
   }
+  int Face::DotInside(std::vector<Vector3> v, float dot_x,const float dot_y){
+    std::vector<float> tmp_result;
+    Vector2 dot_p(dot_x,dot_y);
+    for(int i=0;i<vertex_index.size();i++){
+      Vector2 vertex_sub_vertex = v[(i+1)%3] - v[i];
+      Vector2 dot_p_sub_vertex = (Vector3)dot_p - v[i];
+      tmp_result.push_back(Cross(vertex_sub_vertex,dot_p_sub_vertex));
+    }
+    return IS_EQUAL(tmp_result[0], tmp_result[1], tmp_result[2]) == true ? INSIDE:OUTSIDE;
+  }
 }
--- 完整项目代码/Renderer(vc)/my_models/face.h
+++ 完整项目代码/Renderer(vc)/my_models/face.h
@@ -15,11 +15,16 @@ namespace XX_XZH{
    * 类注释：Face是面片类
    * 该类需要存储v，vt，vn的索引
   */
+  enum IS_INSIDE {OUTSIDE=0,INSIDE,ONLINE};
   class Face{
     public:
       Face() = default;
       virtual ~Face() = default;
       void OutputFace()const;
+      /** 函数注释：判断一个点是否在面内
+       *  输入参数：顶点数组，判断点的坐标
+      */
+      int DotInside(std::vector<Vector3> v, float dot_x,const float dot_y);
       //顶点索引
       std::vector<int> vertex_index;
       //顶点纹理索引
